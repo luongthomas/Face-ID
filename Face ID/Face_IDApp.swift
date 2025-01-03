aaaef2e260323a0b0952da1a1ddcf0a5d66d6396
Thomas Luong <luongt@me.com>
2025-01-03 14:56:27 -0800

    Initial commit

---
+++ Face ID/Face_IDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Face_IDApp.swift
+//  Face ID
+//
+//  Created by Thomas Luong on 1/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Face_IDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Face IDTests/Face_IDTests.swift
@@ -0,0 +1,17 @@
+//
+//  Face_IDTests.swift
+//  Face IDTests
+//
+//  Created by Thomas Luong on 1/3/25.
+//
+
+import Testing
+@testable import Face_ID
+
+struct Face_IDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
